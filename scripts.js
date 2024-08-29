e4fc7b796091fb0695fabe7321739434a6175de2
Ziyi Guan <38952744+ZiyiGuan@users.noreply.github.com>
2024-08-29 19:27:23 +0200

    edit

--- scripts.js
+++ scripts.js
@@ -18,3 +18,12 @@ document.addEventListener("DOMContentLoaded", function() {
         number--;
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('show');
+    });
+});
